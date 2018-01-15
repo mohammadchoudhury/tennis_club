d499045d1f22e185290076b337b825567288ef32
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2018-01-15 22:59:46 +0000

    Updated home list view with custom list item

--- app/src/main/java/com/example/mohammad/tennisclub/HomeActivity.java
+++ app/src/main/java/com/example/mohammad/tennisclub/HomeActivity.java
@@ -3,11 +3,14 @@ package com.example.mohammad.tennisclub;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -54,18 +57,14 @@ public class HomeActivity extends AppCompatActivity {
 
         // Dummy data
         String[] data = {
-                "Court 1 - Single Session - 02 Feb 2017 12:00PM",
-                "Court 2 - Group Session - 03 Feb 2017 15:00PM",
-                "Court 1 - Private Session - 05 Feb 2017 18:00PM"
+                "Court 1|Single Session|02 Feb 2017|12:00PM",
+                "Court 2|Group Session|03 Feb 2017|15:00PM",
+                "Court 1|Private Session|05 Feb 2017|18:00PM"
         };
         List<String> bookings = new ArrayList<>(Arrays.asList(data));
-        ListAdapter bookingsAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_selectable_list_item,
-                data
-        );
-        ListView mListView = (ListView) findViewById(R.id.lv_bookings);
-        mListView.setAdapter(bookingsAdapter);
+        LVAdapter bookingsAdapter = new LVAdapter(bookings);
+        ListView bookingsListView = (ListView) findViewById(R.id.lv_bookings);
+        bookingsListView.setAdapter(bookingsAdapter);
     }
 
     @Override
@@ -96,4 +95,50 @@ public class HomeActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private class LVAdapter extends BaseAdapter {
+
+        List<String> bookings;
+
+        LVAdapter(List<String> bookings) {
+            this.bookings = bookings;
+        }
+
+        @Override
+        public int getCount() {
+            return bookings.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return bookings.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return bookings.get(position).hashCode();
+        }
+
+        @Override
+        public View getView(int position, View listItemView, ViewGroup parent) {
+            if (listItemView == null) {
+                listItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            }
+            final String[] booking = getItem(position).split("\\|");
+            ((TextView) listItemView.findViewById(R.id.tv_item_court)).setText(booking[0]);
+            ((TextView) listItemView.findViewById(R.id.tv_item_session_type)).setText(booking[1]);
+            ((TextView) listItemView.findViewById(R.id.tv_item_date)).setText(booking[2]);
+            ((TextView) listItemView.findViewById(R.id.tv_item_time)).setText(booking[3]);
+
+            AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView parent, View v, int position, long id) {
+                    Toast.makeText(parent.getContext(), "Clicked booking " + ++position, Toast.LENGTH_LONG).show();
+                }
+            };
+            ((ListView) parent).setOnItemClickListener(itemClickListener);
+            return listItemView;
+        }
+
+    }
+
 }
