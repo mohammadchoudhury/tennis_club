e4c124ea418c2e38513ba2ad12d52231a42a7356
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2018-01-17 02:12:44 +0000

    Home activity is now uses tab layout

--- app/src/main/java/com/example/mohammad/tennisclub/HomeActivity.java
+++ app/src/main/java/com/example/mohammad/tennisclub/HomeActivity.java
@@ -2,23 +2,22 @@ package com.example.mohammad.tennisclub;
 
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mohammad.tennisclub.model.User;
 import com.github.clans.fab.FloatingActionButton;
-import com.github.clans.fab.FloatingActionMenu;
 import com.github.clans.fab.Label;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,10 +26,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -58,22 +53,18 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        // Create the adapter that will return a fragment for each of the two
+        // primary sections of the activity.
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Dummy data
-        String[] data = {
-                "Court 1|Single Session|02 Feb 2017|12:00PM",
-                "Court 2|Group Session|03 Feb 2017|15:00PM",
-                "Court 1|Private Session|05 Feb 2017|18:00PM"
-        };
-        List<String> bookings = new ArrayList<>(Arrays.asList(data));
-        LVAdapter bookingsAdapter = new LVAdapter(bookings);
-        ListView bookingsListView = (ListView) findViewById(R.id.lv_bookings);
-        bookingsListView.setAdapter(bookingsAdapter);
-        bookingsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
-                Toast.makeText(parent.getContext(), "Clicked booking " + ++position, Toast.LENGTH_LONG).show();
-            }
-        });
+        // Set up the ViewPager with the sections adapter.
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(sectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
     }
 
     @Override
@@ -115,42 +106,22 @@ public class HomeActivity extends AppCompatActivity {
         Snackbar.make(v, sessionType, Snackbar.LENGTH_LONG).show();
     }
 
-    private class LVAdapter extends BaseAdapter {
 
-        List<String> bookings;
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        LVAdapter(List<String> bookings) {
-            this.bookings = bookings;
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new BookingTabFragment().newInstance(position) ;
         }
 
         @Override
         public int getCount() {
-            return bookings.size();
+            return 2;
         }
-
-        @Override
-        public String getItem(int position) {
-            return bookings.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return bookings.get(position).hashCode();
-        }
-
-        @Override
-        public View getView(int position, View listItemView, ViewGroup parent) {
-            if (listItemView == null) {
-                listItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-            }
-            final String[] booking = getItem(position).split("\\|");
-            ((TextView) listItemView.findViewById(R.id.tv_item_court)).setText(booking[0]);
-            ((TextView) listItemView.findViewById(R.id.tv_item_session_type)).setText(booking[1]);
-            ((TextView) listItemView.findViewById(R.id.tv_item_date)).setText(booking[2]);
-            ((TextView) listItemView.findViewById(R.id.tv_item_time)).setText(booking[3]);
-            return listItemView;
-        }
-
     }
 
 }
