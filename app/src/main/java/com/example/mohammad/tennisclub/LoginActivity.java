f0efada4c94b88cfd42a308b583af3c671ff1e46
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2018-01-18 01:29:26 +0000

    Completed reset password functionality

--- app/src/main/java/com/example/mohammad/tennisclub/LoginActivity.java
+++ app/src/main/java/com/example/mohammad/tennisclub/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.example.mohammad.tennisclub;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -32,10 +36,61 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        mEmailEditTextLayout = (TextInputLayout) findViewById(R.id.etl_email);
-        mEmailEditText = (EditText) findViewById(R.id.et_email);
-        mPasswordEditTextLayout = (TextInputLayout) findViewById(R.id.etl_password);
-        mPasswordEditText = (EditText) findViewById(R.id.et_password);
+        mEmailEditTextLayout = findViewById(R.id.etl_email);
+        mEmailEditText = findViewById(R.id.et_email);
+        mPasswordEditTextLayout = findViewById(R.id.etl_password);
+        mPasswordEditText = findViewById(R.id.et_password);
+
+        findViewById(R.id.tv_forgot_password).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                final View alertView = getLayoutInflater().inflate(R.layout.alert_reset_password, null);
+                AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+                alertDialog.setTitle(getString(R.string.text_reset_password));
+                alertDialog.setView(alertView);
+                alertDialog.setCanceledOnTouchOutside(true);
+                alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.text_cancel), (DialogInterface.OnClickListener) null);
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.text_reset), (DialogInterface.OnClickListener) null);
+                alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(final DialogInterface dialog) {
+                        Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                        button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(final View view) {
+                                String emailReset = ((TextInputEditText) alertView.findViewById(R.id.et_email_reset)).getText().toString();
+                                final TextInputLayout emailResetLayout = alertView.findViewById(R.id.etl_email_reset);
+                                if (isValidEmail(emailReset.trim())) {
+                                    ((EditText) findViewById(R.id.et_email)).setText(emailReset);
+                                    mAuth.sendPasswordResetEmail(emailReset)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                        Snackbar.make(v, getString(R.string.text_reset_sent), Snackbar.LENGTH_SHORT).show();
+                                                        dialog.dismiss();
+                                                    } else {
+                                                        if (task.getException() instanceof FirebaseAuthInvalidUserException) {
+                                                            emailResetLayout.setError(getString(R.string.error_user_does_not_exist));
+                                                        } else {
+                                                            Snackbar.make(view, "ERROR: " + task.getException(), Snackbar.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                }
+                                            });
+                                } else {
+                                    emailResetLayout.setError(getString(R.string.error_email));
+                                }
+                            }
+                        });
+                    }
+                });
+
+                alertDialog.show();
+                ((TextInputEditText) alertView.findViewById(R.id.et_email_reset))
+                        .setText(((EditText) findViewById(R.id.et_email)).getText().toString());
+            }
+        });
 
         ((Button) findViewById(R.id.btn_login)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,7 +159,6 @@ public class LoginActivity extends AppCompatActivity {
 
 
     private boolean isValidEmail(String email) {
-        final String pattern = "^[_AZaz09\\+]+(\\.[_AZaz09]+)*@[AZaz09]+(\\.[AZaz09]+)*(\\.[AZaz]{2,})$";
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
