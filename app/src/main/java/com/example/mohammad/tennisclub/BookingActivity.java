83ac4d9c919e1eff58a1b9c8d4ad913e8c1c9c64
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2018-03-07 22:46:14 +0000

    Created booking activity

---
+++ app/src/main/java/com/example/mohammad/tennisclub/BookingActivity.java
@@ -0,0 +1,18 @@
+package com.example.mohammad.tennisclub;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+
+        ((BottomNavigationView) findViewById(R.id.bottom_navigation)).setSelectedItemId(R.id.nav_normal);
+    }
+}
